03ca055d0b68fa270589558b931ae4fc71d2d308
Simon vom Eyser <simon.vom.eyser@gmail.com>
2018-09-05 13:51:24 +0200

    Add colors to warning

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+var colors = require('colors/safe');
 
 try {
     
-    var config = require('../../.ssh-commands')
+    var config = require('../../.ssh-commandss')
 
 } catch (error) {
 
-    console.warn("Simple SSH Commands: You are missing an '.ssh-commands' file in your root directory.")
+    console.warn(colors.yellow("Simple SSH Commands: You are missing an '.ssh-commands' file in your root directory."));
     
 } 
 
