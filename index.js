c3970bec8ac09bf4b54639bf04821dd8c394ae2f
Simon vom Eyser <simon.vom.eyser@gmail.com>
2018-09-05 13:46:38 +0200

    Add simple reading of config

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Binary working");
+
+try {
+    
+    var config = require('../../.ssh-commands')
+
+} catch (error) {
+
+    console.warn("Simple SSH Commands: You are missing an '.ssh-commands' file in your root directory.")
+    
+} 
+
+return;
