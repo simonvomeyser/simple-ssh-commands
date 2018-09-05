32b4a54ff0e83fc0263ab6dd2a954c182715b6c6
Simon vom Eyser <simon.vom.eyser@gmail.com>
2018-09-05 14:40:08 +0200

    Add first working version

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
 var colors = require('colors/safe');
+const prompt = require('prompt');
+const node_ssh = require('node-ssh');
+const ssh = new node_ssh();
+
+require('dotenv').config();
 
 try {
     
-    var config = require('../../.ssh-commandss')
+    var config = require(process.cwd()+'/.ssh-commands.json');
 
 } catch (error) {
 
     console.warn(colors.yellow("Simple SSH Commands: You are missing an '.ssh-commands' file in your root directory."));
+    return;
     
-} 
+}
 
-return;
+// Credendtials used to log in
+const credentials = {
+    username: config.username,
+    host: config.host,
+    password: process.env.DEPLOY_PASSWORD
+  };
+const commands = config.commands;
+  
+  // When the password exists (in .env file) don't ask for password
+  if (credentials.password) {
+    executeDeploy(credentials);
+  } else {
+    askForPassword(credentials)
+      .then(credentialsWithPassword => executeDeploy(credentialsWithPassword))
+      .catch(error => console.log('An error occured'));
+  }
+  
+  /**
+   * Prompts for password
+   * @param {object} credentials
+   * @return {Promise}
+   */
+  function askForPassword(credentials) {
+    return new Promise((resolve, reject) => {
+      console.log(
+        'Enter Password for ' +
+        credentials.username +
+        '@' +
+        credentials.host +
+        ' (Or add DEPLOY_PASSWORD to your .env file)'
+      );
+  
+      return prompt.get(['password'], function (err, result) {
+        if (err) reject(err);
+  
+        credentials.password = result.password;
+        resolve(credentials);
+      });
+    });
+  }
+  
+  /**
+   * Connects to the server with credentials and prints output to console
+   * @param {object} credentials
+   */
+  function executeDeploy(credentials) {
+    ssh.connect(credentials).then(function () {
+      ssh.execCommand(commands.join(' && ') + ' \n').then(function (result) {
+        console.log(result.stdout);
+        console.log(result.stderr);
+        ssh.dispose();
+      });
+    });
+  }
+  
